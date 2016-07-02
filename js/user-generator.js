f715b7e61299696d2761b2b0cd30be1b0bb27fac
Taylor Pokoj <pokojt@gmail.com>
2016-07-02 16:55:06 -0700

    Fix host nationality value for ajax request

--- js/user-generator.js
+++ js/user-generator.js
@@ -2,12 +2,51 @@ exports.Invitation = function(guestGender, guestNationality, hostGender, hostNat
   this.guestGender = guestGender;
   this.guestNationality = guestNationality;
   this.hostGender = hostGender;
-  this.hostNationaltiy = hostNationality;
-}
+  this.hostNationality = hostNationality;
+};
+
+
+// exports.Invitation.prototype.getGuest = function() {
+//   $.get("https://randomuser.me/api/?gender='+ this.guestGender+'&nat='+this.guestNationality").then(function(data) {
+//     console.log(data);
+//     var guestFirstName = data.results[0].name.first;
+//     var guestLastName = data.results[0].name.last;
+//
+//     var guestName = guestFirstName + " " + guestLastName;
+//
+//     $("#guestName").text(guestName);
+//     console.log(guestName);
+//     console.log(this.guestGender);
+//   });
+// };
+//
+// exports.Invitation.prototype.getHost = function() {
+//   $.get("https://randomuser.me/api/?gender='+this.hostGender+'&nat='+this.hostNationality").then(function(data) {
+//     console.log(data);
+//     console.log(this.hostGender);
+//
+//     var hostStreet = data.results[0].location.street;
+//     var hostCity =  data.results[0].location.city;
+//     var hostState = data.results[0].location.state;
+//     var hostZip = data.results[0].location.postcode;
+//
+//
+//     var hostFirstName = data.results[0].name.first;
+//     var hostLastName = data.results[0].name.last;
+//
+//     var hostName = hostFirstName + " " +hostLastName;
+//
+//
+//     $("#hostAddress").text(hostStreet + ", " + hostCity + ", " + hostState + " " + hostZip);
+//     $("#hostName").text(hostName);
+//     console.log(hostName);
+//
+//   });
+// };
 
 exports.Invitation.prototype.getGuest = function() {
   $.ajax({
-    url: "https://randomuser.me/api/?gender="+ this.guestGender+"&nat="+this.guestNationality,
+    url: "https://randomuser.me/api/?gender="+this.guestGender+"&nat="+this.guestNationality,
     dataType: 'json',
     success: function(data){
       console.log(data);
@@ -23,26 +62,22 @@ exports.Invitation.prototype.getGuest = function() {
 }
 
 
-
 exports.Invitation.prototype.getHost = function() {
   $.ajax({
-    url: "https:randomuser.me/api/?gender="+ this.hostGender+"&nat="+this.hostNationality,
+    url: "https://randomuser.me/api/?gender="+this.hostGender+"&nat="+this.hostNationality,
     dataType: 'json',
     success: function(data){
       console.log(data);
-      console.log(this.hostGender);
 
       var hostStreet = data.results[0].location.street;
       var hostCity =  data.results[0].location.city;
       var hostState = data.results[0].location.state;
       var hostZip = data.results[0].location.postcode;
 
-
       var hostFirstName = data.results[0].name.first;
       var hostLastName = data.results[0].name.last;
 
       var hostName = hostFirstName + " " +hostLastName;
-
 
       $("#hostAddress").text(hostStreet + ", " + hostCity + ", " + hostState + " " + hostZip);
       $("#hostName").text(hostName);
